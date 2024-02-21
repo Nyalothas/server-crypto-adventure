411ee831a4afa5befc1ee7ccaa2390620ea136bd
Nyalothas <nyalothas@gmail.com>
2024-02-21 23:00:08 +0200

    added files

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const routes = require('./src/routes');
+const coinRoutes = require('./src/routes/coinRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+	res.send("<h2>It's Working!</h2>");
+});
+
+app.use(bodyParser.json());
+app.use('/api', routes);
+app.use('/api/coins', coinRoutes);
+
+app.listen(PORT, () => {
+	console.log(`API is listening on port ${PORT}`);
+});
---
+++ src/controllers/coinController.js
@@ -0,0 +1,64 @@
+const coinService = require("../services/coinService");
+
+const getAllCoins = (req, res) => {
+	const allCoins = coinService.getAllCoins();
+	res.send({ status: "OK", data: allCoins });
+};
+
+const getOneCoin = (req, res) => {
+	const {
+		params: { coinId },
+	} = req;
+	if (!coinId) {
+		return;
+	}
+	const coin = coinService.getOneCoin(coinId);
+	res.send({ status: "OK", data: coin });
+};
+
+const createNewCoin = (req, res) => {
+	const { body } = req;
+	if (
+		!body.name ||
+		!body.price
+	) {
+		return;
+	}
+	const newCoin = {
+		name: body.name,
+		price: body.price
+	};
+	const createdCoin = coinService.createNewCoin(newCoin);
+	res.status(201).send({ status: "OK", data: createdCoin });
+};
+
+const updateOneCoin = (req, res) => {
+	const {
+		body,
+		params: { coinId },
+	} = req;
+	if (!coinId) {
+		return;
+	}
+	const updatedCoin = coinService.updateOneCoin(coinId, body);
+	res.send({ status: "OK", data: updatedCoin });
+};
+
+const deleteOneCoin = (req, res) => {
+	const {
+		params: { coinId },
+	} = req;
+	if (!coinId) {
+		return;
+	}
+	coinService.deleteOneCoin(coinId);
+	res.status(204).send({ status: "OK" });
+};
+
+module.exports = {
+	getAllCoins,
+	getOneCoin,
+	createNewCoin,
+	updateOneCoin,
+	deleteOneCoin,
+};
