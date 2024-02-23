7f33dfa6e01a9c3641ff2d6141fa70dde36e8a9b
Nyalothas <nyalothas@gmail.com>
2024-02-23 15:23:30 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require('cors');
 
 const routes = require('./src/routes');
 const coinRoutes = require('./src/routes/coinRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 80;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
 	res.send("<h2>It's Working!</h2>");
