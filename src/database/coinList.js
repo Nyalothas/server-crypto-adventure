17139acbaf7a156f3e05dbeca7b270719f9cebf2
Nyalothas <nyalothas@gmail.com>
2024-03-22 17:11:53 +0200

    Create coinList.js

---
+++ src/database/coinList.js
@@ -0,0 +1,60 @@
+const DB = require("./dbList.json");
+const { saveToDatabase } = require("./utils");
+
+const getAllCoins = () => {
+	return DB.coins;
+};
+
+const getOneCoin = (coinId) => {
+	const coin = DB.coins.find((coin) => coin.id === coinId);
+	if (!coin) {
+		return;
+	}
+	return coin;
+};
+
+const createNewCoin = (newCoin) => {
+	const isAlreadyAdded =
+		DB.coins.findIndex((coin) => coin.name === newCoin.name) > -1;
+	if (isAlreadyAdded) {
+		return;
+	}
+	DB.coins.push(newCoin);
+	saveToDatabase(DB);
+	return newCoin;
+};
+
+const updateOneCoin = (coinId, changes) => {
+	const indexForUpdate = DB.coins.findIndex(
+		(coin) => coin.id === coinId
+	);
+	if (indexForUpdate === -1) {
+		return;
+	}
+	const updatedCoin = {
+		...DB.coins[indexForUpdate],
+		...changes
+	};
+	DB.coins[indexForUpdate] = updatedCoin;
+	saveToDatabase(DB);
+	return updatedCoin;
+};
+
+const deleteOneCoin = (coinId) => {
+	const indexForDeletion = DB.coins.findIndex(
+		(coin) => coin.id === coinId
+	);
+	if (indexForDeletion === -1) {
+		return;
+	}
+	DB.coins.splice(indexForDeletion, 1);
+	saveToDatabase(DB);
+};
+
+module.exports = {
+	getAllCoins,
+	createNewCoin,
+	getOneCoin,
+	updateOneCoin,
+	deleteOneCoin,
+};
